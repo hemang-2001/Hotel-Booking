9775ac822af6fac497d3bea2eb9ddc0beb9dce62
Hemang Bairwa <hamang2001@gmail.com>
2025-06-28 20:15:07 +0530

    fix: Correct table header from 'Name' to 'Room Type' and ensure room data is displayed correctly in ListRoom component

--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -14,7 +14,7 @@ const ListRoom = () => {
             <table className='w-full'>
                 <thead className='bg-gray-50' >
                         <tr className='bg-gray-100 border-b'>
-                            <th className='py-3 px-4 text-gray-800 font-medium'>Name</th>
+                            <th className='py-3 px-4 text-gray-800 font-medium'>Room Type</th>
                             <th className='py-3 px-4 text-gray-800 font-medium max-sm:hidden'>Facility</th>
                             <th className='py-3 px-4 text-gray-800 font-medium'>Price / night</th>
                             <th className='py-3 px-4 text-gray-800 font-medium text-center'>Actions</th> 
@@ -22,8 +22,16 @@ const ListRoom = () => {
                     </thead>
                     <tbody className='text-sm' >
                         {
-                            rooms.map(() =>(
-                                <tr></tr>
+                            rooms.map((item, index) =>(
+                                <tr key={index} >
+                                    <td className='py-3 px-4 text-gray-800 border-t border-gray-300 '>{item.roomType}</td>
+                                    <td className='py-3 px-4 text-gray-800 border-t border-gray-300 max-sm:hidden '>{item.amenities.join(', ')}</td>
+                                    <td className='py-3 px-4 text-gray-800 border-t border-gray-300 '>${item.pricePerNight}</td>
+                                    <td className='text-center border-t border-gray-300  '>
+                                        <button className='px-3 py-1 text-xs bg-blue-500 text-white rounded mx-auto hover:bg-blue-600 transition-all' >Edit</button>
+                                        <button className='px-3 py-1 text-xs bg-red-500 text-white rounded mx-auto hover:bg-red-600 transition-all ml-2' >Delete</button>
+                                    </td>
+                                </tr>
                             ))
                         }
                     </tbody>
